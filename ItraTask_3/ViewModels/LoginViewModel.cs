b1d39c2caa9d87ec1df1aae122bea23cb7b7b1a9
Sidodji <DaniilAlisevich@gmail.com>
2022-02-12 11:49:09 +0300

    Task 3

---
+++ ItraTask_3/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItraTask3.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
